41016142a1cd0d2719bd08d7d0e8519917ec70f6
adelatatar <116831078+adelatatar@users.noreply.github.com>
2023-04-02 21:29:02 +0300

    relations between tables

---
+++ src/main/java/com/example/ps_project/entities/Enrollments.java
@@ -0,0 +1,29 @@
+package com.example.ps_project.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Enrollments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Enrollments {
+    @GeneratedValue
+    @Id
+    @Column(name="ID")
+    private Integer id;
+
+    @NotNull
+    @Column(name="USERID")
+    private Integer userID;
+
+    @NotNull
+    @Column(name="COURSEID")
+    private Integer courseID;
+}
